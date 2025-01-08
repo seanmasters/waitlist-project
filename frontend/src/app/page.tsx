fc92dd1ff2bdc3e67f323ee794b4f3553c885701
Shrijan Dangol <shrijan.dangol@protonmail.com>
2025-01-08 20:34:46 +0545

    third commit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
+      <div>
       <h1>Home</h1>
       <Image src="/logo.png" alt="logo" width={200} height={200} />
+    </div>
+    <div>
+      <input></input>
+    </div>
+    </div>
   );
 }
